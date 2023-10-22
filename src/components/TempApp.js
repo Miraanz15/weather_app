0233b9995bd0de943e356d89942aa8a66fbdf33c
Miraanz15 <miraanejaz15@gmail.com>
2023-10-22 17:53:59 +0530

    Funtionalities added

--- src/components/TempApp.js
+++ src/components/TempApp.js
@@ -5,7 +5,7 @@ const TempApp = () => {
     const [city, setCity] = useState('');   // getting value from user through input
     const [search, setSearch] = useState('');    // setting value to get respective city from api
     const [value, setValue] = useState({});            // setting api data to be used in the card
-    const [available, setAvailable] = useState(false); 
+    const [available, setAvailable] = useState(false);  // to check if api received is valid or not
     
      
 
@@ -41,39 +41,24 @@ const TempApp = () => {
   return (
     <>
     <div className={styles.box}>
+        <h1 className={styles.headingTop}>Live Weather</h1>
         <div className={styles.inputData}>
+            <label for="inputField" id={styles.inputFieldLabel}>City <br /></label>
             <input 
-            className={styles.inputField}
+            id={styles.inputField}
             type='search' 
             value={city} 
             placeholder='Enter the city' 
             onChange={onChangeHandler}/>
         </div>
-        {/*
-        {available ? (
-        <div className={styles.info}>
-          <h2 className={styles.location}>
-             <i className="fa-solid fa-street-view"></i> {value.name}
-          </h2>
-          <h2 className={styles.temp}>{value.main.temp} Celsius</h2>
-          <h3 className={styles.tempmin_max}>Min | Max</h3>
-        </div>) : (<div> </div>)}
-            */}
 
         {available ?  (<div className={styles.info}>
              <h2 className={styles.location}>
                  <i className="fa-solid fa-street-view"></i>      {value.name}</h2>
-             <h2 className={styles.temp}>{value.main.temp} Celsius</h2>
-             <h3 className={styles.tempmin_max}>Min | Max</h3>
-        </div>) : (<div>Sorry, City not found</div>)}
-         
-  
-        
-        {/*
-        <div className={`${styles.wave} ${styles.one}`}></div> 
-        <div className={`${styles.wave} ${styles.two}`}></div>
-        <div className={`${styles.wave} ${styles.three}`}></div>
-*/}
+             <h2 className={styles.temp}>{(value.main.temp - 273.15).toFixed(2)} &deg; Cel</h2>
+             <h3 className={styles.tempmin_max}>Min {(value.main.temp_min - 273.15).toFixed(2)} &deg; Cel | Max {(value.main.temp_max - 273.15).toFixed(2)} &deg; Cel</h3>
+        </div>) : (search ? (<div className={styles.message}>Sorry, City not found</div>) : (<div className={styles.message}>Please enter the City name</div>)  )}
+
     </div>
     </>
   )
