6e7bf0155f52b7faedbfa01ef260c15b57565994
Miraanz15 <miraanejaz15@gmail.com>
2023-10-22 16:28:36 +0530

    Feature added

--- src/components/TempApp.js
+++ src/components/TempApp.js
@@ -2,8 +2,13 @@ import React, {useState, useEffect} from 'react';
 import styles from './css/TempApp.module.css';
 
 const TempApp = () => {
-    const [city, setCity] = useState('');
-    const [search, setSearch] = useState("Mumbai");
+    const [city, setCity] = useState('');   // getting value from user through input
+    const [search, setSearch] = useState('');    // setting value to get respective city from api
+    const [value, setValue] = useState({});            // setting api data to be used in the card
+    const [available, setAvailable] = useState(false); 
+    
+     
+
 
     useEffect(() => {
          const fetchApi = async () =>{
@@ -11,16 +16,27 @@ const TempApp = () => {
             const response = await fetch(url);
             const data = await response.json();
             console.log(data);
-
+            if(Object.keys(data).length > 2 ){
+                setValue(data);
+                setAvailable(true);
+            }
+            else{
+                setValue({});
+                setAvailable(false);
+            }
+            
          }
 
         fetchApi();
-    }, []);
-  
+    }, [search]);
+
+    
     const onChangeHandler = (event) => {
           console.log(event.target.value)
           setCity(event.target.value)
+          setSearch(event.target.value);
     }
+
 
   return (
     <>
@@ -33,15 +49,31 @@ const TempApp = () => {
             placeholder='Enter the city' 
             onChange={onChangeHandler}/>
         </div>
+        {/*
+        {available ? (
         <div className={styles.info}>
+          <h2 className={styles.location}>
+             <i className="fa-solid fa-street-view"></i> {value.name}
+          </h2>
+          <h2 className={styles.temp}>{value.main.temp} Celsius</h2>
+          <h3 className={styles.tempmin_max}>Min | Max</h3>
+        </div>) : (<div> </div>)}
+            */}
+
+        {available ?  (<div className={styles.info}>
              <h2 className={styles.location}>
-                 <i className="fa-solid fa-street-view"></i> {city }</h2>
-             <h2 className={styles.temp}>Celsius</h2>
+                 <i className="fa-solid fa-street-view"></i>      {value.name}</h2>
+             <h2 className={styles.temp}>{value.main.temp} Celsius</h2>
              <h3 className={styles.tempmin_max}>Min | Max</h3>
-        </div>  
-        <div className={`${styles.wave} ${styles.one}`}></div>
+        </div>) : (<div>Sorry, City not found</div>)}
+         
+  
+        
+        {/*
+        <div className={`${styles.wave} ${styles.one}`}></div> 
         <div className={`${styles.wave} ${styles.two}`}></div>
         <div className={`${styles.wave} ${styles.three}`}></div>
+*/}
     </div>
     </>
   )
